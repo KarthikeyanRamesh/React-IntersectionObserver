d08c81cde9e0d2bc9879940a171e9dc554e85540
KarthikeyanRamesh <karthisujatha10@gmail.com>
2022-12-12 13:52:17 +0530

    interaction observer in react project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef} from "react";
+import useElementOnScreen from "./hooks/useElementOnScreen";
 
 function App() {
+  const targetRef = useRef(null);
+  const isVisible = useElementOnScreen({
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+  }, targetRef);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    <h1 className="header">
+      <p>{!isVisible ? 'not in viewport': 'in viewport'}</p>
+    </h1>
+    <div className="gap"></div>
+    <img alt="alt-text" src="Img/friend.jpeg" ref={targetRef} />
+    </>
   );
 }
 
